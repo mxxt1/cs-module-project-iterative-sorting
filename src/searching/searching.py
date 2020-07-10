425cc0ecfa32d751fbc85f50d8287fe781518fd4
Matt Gill <matt@dustcloudlabs.com>
2020-07-10 18:51:42 -0400

    searching complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,11 +8,22 @@ def linear_search(arr, target):
 
     return -1   # not found
 
-
+import math
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    left = 0
+    right = len(arr)-1
+
+    while left <= right:
+        pivot = math.ceil((left+right)//2)
+        if arr[pivot] == target:
+            return pivot
+       
+        if arr[pivot] < target:
+            left = pivot + 1    
+        elif arr[pivot] > target:
+            right = pivot-1
 
 
     return -1  # not found
